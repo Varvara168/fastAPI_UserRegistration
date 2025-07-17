a2b8a8d7e191d269a00305558e972090d4fc03f1
Varvara <v20004565@gmail.com>
2025-07-17 17:02:41 +0400

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,12 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
+
+class TokenData(BaseModel):
+    sub: str | None = None
+    
 class UserCreate(BaseModel):
-    username: str
+    email: EmailStr
     password: str
 
-class UserReturn(BaseModel):
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class UserOut(BaseModel):
     id: int
-    username: str
+    email: EmailStr
 
     class Config:
         orm_mode = True
